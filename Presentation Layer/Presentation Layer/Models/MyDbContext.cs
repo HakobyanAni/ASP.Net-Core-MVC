29efc93f0e1a2882180ce85c1e518558b76f664f
annie .h.h <annie2016l@gmail.com>
2019-03-29 23:39:37 +0400

    Authetitcation, Authorization(not completed)

---
+++ Presentation Layer/Presentation Layer/Models/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Presentation_Layer.Models
+{
+    public class MyDbContext : IdentityDbContext<User>
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
