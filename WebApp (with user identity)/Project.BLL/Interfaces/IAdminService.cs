30013f98f6b9c3e232faa444856ecc4e43d5095a
Ani Hakobyan <Ani.Hakobyan@4p1p.com>
2019-06-17 15:49:10 +0400

    some changes in Logic layer

--- WebApp (with user identity)/Project.BLL/Interfaces/IAdminService.cs
+++ WebApp (with user identity)/Project.BLL/Interfaces/IAdminService.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Project.DAL.Entities;
 using Project.Web.Models;
 
 namespace Project.BLL.Interfaces
 {
     interface IAdminService
     {
-        Task<string> CreateClient(ClientModel clientModel, UserModel userModel);
+        IQueryable<ClientEntity> GetAllClients();
+        Task<ClientModel> GetClientById(int id);
+        Task<string> CreateClient(ClientModel clientModel, int userId);
         Task<string> EditClientName(int clientId, string nameToBe, UserModel userModel);
         Task<string> DeleteClient(int clientId, UserModel userModel);
     }
